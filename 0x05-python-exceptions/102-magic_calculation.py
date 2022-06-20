d4e971b96eeb77aedc64b58b616f39cb5dd4711a
iamyunusali <aliyunus291@gmail.com>
2022-06-21 01:16:57 +0300

    corrections

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-
-def safe_function(fct, *args):
-    try:
-        return fct(*args)
-    except (IndexError, ValueError, ZeroDivisionError, TypeError) as e:
-        sys.stderr.write(f"Exception: {e}\n".format(e))
-        return None
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += (a ** b) / i
+        except Exception:
+            result = b + a
+            break
+        return result
